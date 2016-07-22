92ff6aad75b1429f47b7eeb25fb224ed5dc9095b
Henrik Lissner <henrik@lissner.net>
2016-07-21 23:01:43 -0400

    Refactor gulpfile; add eslintConfig; update typings

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
-const gulp    = require('gulp'),
-      changed = require('gulp-changed'),
-      rename  = require("gulp-rename"),
-      source  = require('vinyl-source-stream');
-
+const gulp    = require("gulp"),
+      util    = require("gulp-util"),
+      changed = require("gulp-changed"),
+      merge   = require("merge-stream");
 // pug
-const pug     = require('gulp-pug');
-
+const pug     = require("gulp-pug");
 // sass
-const sass    = require('gulp-sass'),
-      bourbon = require('node-bourbon').includePaths;
-
+const sass    = require("gulp-sass"),
+      bourbon = require("node-bourbon").includePaths;
 // typescript
-const ts         = require('gulp-typescript'),
-      browserify = require('browserify'),
-      tsify      = require('tsify');
-
-var tsProject = ts.createProject('tsconfig.json');
+const ts         = require("gulp-typescript"),
+      tsProject  = ts.createProject("tsconfig.json");
+// js
+const browserify = require("browserify"),
+      source     = require("vinyl-source-stream"),
+      buffer     = require("vinyl-buffer"),
+      uglify     = require("gulp-uglify");
 
 //
 const dir = {
@@ -26,49 +25,67 @@ const dir = {
 
 
 // Tasks
-gulp.task('sass', () => {
+gulp.task("sass", () => {
     const dest = dir.build + "/css";
-    gulp.src(dir.node + "normalize.css/normalize.css").pipe(gulp.dest(dest));
-    return gulp.src(dir.source + "/stylesheets/**.scss")
-        .pipe(changed(dest, {extension: '.scss'}))
-        .pipe(
-            sass({includePaths: bourbon, outputStyle: 'compressed', sourcemap:true})
-                .on('error', sass.logError)
-        )
+
+    let norm = gulp.src("normalize.css/normalize.css", {cwd: dir.node})
         .pipe(gulp.dest(dest));
+
+    let scss = gulp.src("stylesheets/**.scss", {cwd: dir.source})
+        .pipe(changed(dest, {extension: ".scss"}))
+        .pipe(sass({
+                includePaths: bourbon,
+                outputStyle: "compressed",
+                sourcemap: true
+            }).on("error", sass.logError))
+        .pipe(gulp.dest(dest));
+
+    return merge(norm, scss);
 });
 
-gulp.task('ts', () => {
-    return browserify({entries: ["app.ts"], basedir: dir.source, cache: {}, packageCache: {}, plugin: [tsify]})
-        .exclude("@angular/platform-browser-dynamic")
-        .bundle()
-        .on('error', (err) => { console.error(err.toString()); })
-        .pipe(source("app.js"))
-        .pipe(gulp.dest(dir.build + "/app"));
+gulp.task("ts", () => {
+    let src = gulp.src(["typings/index.d.ts", dir.source + "**/*.ts"])
+        .pipe(ts(tsProject))
+        .pipe(gulp.dest(dir.build + "app/"));
 
+    if (util.env.release) {
+        let brs = browserify(dir.build + "app/app.js")
+            .bundle()
+            .on("error", (err) => { console.error(err.toString()); })
+            .pipe(source("app.js"))
+            .pipe(buffer())
+            .pipe(uglify())
+            .pipe(gulp.dest(dir.build + "/app/"));
+
+        return merge(src, brs);
+    }
+
+    return src;
 });
 
-gulp.task('pug', () => {
-    let locals = {locals: {env: process.env.ENV}};
+gulp.task("pug", () => {
+    let locals = {locals: {release: util.env.release}};
 
-    gulp.src(dir.source + "views/index.pug")
-        .pipe(changed(dir.build, {extension: '.pug'}))
+    let index = gulp.src("views/index.pug", {cwd: dir.source})
+        .pipe(changed(dir.build, {extension: ".pug"}))
         .pipe(pug(locals))
         .pipe(gulp.dest(dir.build));
 
-    return gulp.src([dir.source + "views/**/*.pug", "!" + dir.source + "views/index.pug"], {base: dir.source})
-        .pipe(changed(dir.build, {extension: '.pug'}))
+    let views = gulp.src(["views/**/*.pug", "!views/index.pug"], {cwd: dir.source, base: dir.source})
+        .pipe(changed(dir.build, {extension: ".pug"}))
         .pipe(pug(locals))
         .pipe(gulp.dest(dir.build));
+
+    return merge(index, views);
 });
 
-gulp.task('watch', () => {
+gulp.task("watch", () => {
     // TODO add browserSync
-    gulp.watch(dir.source + "**/*.ts", ['ts']);
-    gulp.watch(dir.source + "**/*.pug", ['pug']);
-    gulp.watch(dir.source + "stylesheets/**/*.scss", ['sass']);
+    gulp.watch(dir.source + "**/*.ts", ["ts"]);
+    gulp.watch(dir.source + "**/*.pug", ["pug"]);
+    gulp.watch(dir.source + "stylesheets/**/*.scss", ["sass"]);
 });
 
 
 // Default task
-gulp.task('default', ['ts', 'sass', 'pug']);
+gulp.task("default", ["ts", "sass", "pug"]);
